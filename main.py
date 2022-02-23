664c4d1a7686d2a1b781e058a6b86bbc42f7e4bd
Marc Baetica <marc.baetica@gmail.com>
2022-02-23 16:13:09 -0500

    Api setup.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Test!')
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+app.run(port=5000)
+# Alternatively:
+# export FLASK_APP=hello.py
+# flask run
