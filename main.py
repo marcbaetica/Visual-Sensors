b676b2bf3c8908e8e022506027add8951a3a179f
Marc Baetica <marc.baetica@gmail.com>
2022-02-25 19:31:05 -0500

    Proper return code.

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ def hello_world():
 def test_post():
     input_json = request.get_json(force=True)
     response = {'text': input_json['text']}
-    # return jsonify(response)
-    return '', 204
+    return response, 204
 
 
 app.run(port=5000)
